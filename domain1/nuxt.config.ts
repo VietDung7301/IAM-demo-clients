41af9e25c79e6cd455416be75527e47c82e5817d
VietDung <vietdung.7301@gmail.com>
2024-01-26 05:26:45 +0700

    Add sign sign on

--- domain1/nuxt.config.ts
+++ domain1/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       AUTHORIZATION_ENDPOINT: process.env.AUTHORIZATION_ENDPOINT,
       TOKEN_ENDPOINT: process.env.TOKEN_ENDPOINT,
       OAUTH_RESPONSE_TYPE: 'code',
-      REDIRECT_URI: process.env.LOGIN_CALLBACK || '/login/callback',
+      REDIRECT_URI: process.env.LOGIN_CALLBACK,
       CLIENT_ID: process.env.CLIENT_ID,
       CLIENT_SECRET: process.env.CLIENT_SECRET
     }
--- domain1/pages/login/callback.vue
+++ domain1/pages/login/callback.vue
@@ -1,5 +1,6 @@
 <template>
-	<iframe src="https://www.w3schools.com" title="W3Schools Free Online Web Tutorials"></iframe>
+	<iframe id="save-iframe" src="http://localhost:3000/login/save_token" title="iam"></iframe>
+	<iframe id="get-iframe" src="http://localhost:3000/login/send_token" title="iam"></iframe>
 </template>
 <script setup>
 const config = useRuntimeConfig()
@@ -37,6 +38,34 @@ if (params.code) {
 	} else {
 		access_token.value = data.value.access_token;
 		refresh_token.value = data.value.refresh_token;
+
+		if (process.browser) {
+			const iframe = document.querySelector("#save-iframe")
+			
+			const token = {
+				access_token: access_token.value,
+				refresh_token: refresh_token.value
+			}
+
+			const token_message = JSON.parse(JSON.stringify(token))
+
+			console.log('token_message', token_message)
+
+			iframe.contentWindow.postMessage(token_message, '*');
+		}
+	}
+} else if (params.access_token) {
+	console.log('chay vao day roi')
+	if (process.browser) {
+		window.onmessage = function(e) {
+			console.log('day la token nhan tu central', e)
+			if (e.data.access_token) {
+				access_token.value = e.data.access_token
+			}
+			if (e.data.refresh_token) {
+				refresh_token.value = e.data.refresh_token
+			}
+		};
 	}
 }
 </script>
